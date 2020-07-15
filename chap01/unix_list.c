dca7254f96c2161b1413e3342443ccc103165dad
stalkervr <stalkervr@gmail.com>
2020-07-15 13:50:39 +0700

    new14 new15

--- chap01/unix_list.c
+++ chap01/unix_list.c
@@ -47,7 +47,17 @@ int main() {
         int family = address->ifa_addr->sa_family;
         if (family == AF_INET || family == AF_INET6) {
 
+            printf("%s\t", ->sa_family;
+        if (family == AF_INET || family == AF_INET6) {
+
             printf("%s\t", address->ifa_name);
+            printf("%s\t", family == AF_INET ? "IPv4" : "IPv6");
+
+            char ap[100];
+            const int family_size = family == AF_INET ?
+                sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
+            getnameinfo(address->ifa_addr,
+                    family_size, ap, sizeof(ap), 0, 0, NI_NUMERICHOST);address->ifa_name);
             printf("%s\t", family == AF_INET ? "IPv4" : "IPv6");
 
             char ap[100];
